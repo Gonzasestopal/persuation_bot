3376b901ec3110416c708f4e8129593a66c85f53
gonzasestopal <gonzasestopal@gmail.com>
2025-08-19 13:15:19 -0600

    feat(llm): dummy debate method

--- app/adapters/llm/dummy.py
+++ app/adapters/llm/dummy.py
@@ -1,7 +1,12 @@
-from app.domain.models import Conversation
+from typing import List
+
+from app.domain.models import Conversation, Message
 from app.domain.ports.llm import LLMPort
 
 
 class DummyLLMAdapter(LLMPort):
     async def generate(self, conversation: Conversation) -> str:
         return f"I am a bot that defends {conversation.topic} and im side {conversation.side}"
+
+    async def debate(self, messages: List[Message]) -> str:
+        return f"After considering your last {len(messages)} messages, this is not totally correct."
